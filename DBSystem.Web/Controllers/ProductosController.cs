88c84e0f58aa261011abf6e9ea6181ffab7755c4
crackper <samuel.mestanza@hotmail.com>
2013-10-25 17:30:05 -0500

    buscar productos listo....

---
+++ DBSystem.Web/Controllers/ProductosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBSystem.Core.Domain;
+using DBSystem.Services;
+
+namespace DBSystem.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        IProductoService _productoService;
+
+        public ProductosController()
+        {
+            _productoService = new ProductoService();
+        }
+
+        //
+        // GET: /Productos/
+
+        public ActionResult Index()
+        {
+            var lista = _productoService.GetAllProductos("");
+            return View(lista);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string criterio="")
+        {
+            var lista = _productoService.GetAllProductos(criterio);
+            return View(lista);
+        }
+
+    }
+}
